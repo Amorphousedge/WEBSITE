4629c3dd6db05d41c9d7920e263d840af6c4c133
Amorphousedge <163412821+Amorphousedge@users.noreply.github.com>
2024-06-01 01:15:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+// script.js
+const cryptogramInput = document.getElementById('cryptogram-input');
+const submitButton = document.getElementById('submit-button');
+const resultDiv = document.getElementById('result');
+
+submitButton.addEventListener('click', () => {
+    const inputText = cryptogramInput.value;
+    const decryptedText = decrypt(inputText);
+    resultDiv.innerText = decryptedText;
+});
+
+function decrypt(text) {
+    const cipherText = '3iz8Kqfug8xZCXSbNKZk1e';
+    const key = {
+        '3': 'A',
+        'i': 'B',
+        'z': 'C',
+        '8': 'D',
+        'K': 'E',
+        'q': 'F',
+        'f': 'G',
+        'u': 'H',
+        'g': 'I',
+        'x': 'J',
+        'Z': 'K',
+        'C': 'L',
+        'X': 'M',
+        'S': 'N',
+        'b': 'O',
+        'N': 'P',
+        'K': 'Q',
+        'Z': 'R',
+        'k': 'S',
+        '1': 'T',
+        'e': 'U'
+    };
+    let decryptedText = '';
+
+    for (let i
